2243fec7603eb44513e29ae9fcb05b87ce4fb3ce
Your Name <you@example.com>
2019-05-07 14:18:33 -0400

    setup db with User Player and Post seed. Communicated console info successfully.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
     has_secure_password
 
-    validates :username, presence: true, uniqueness: true
+    validates :name, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: true
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-    
+
     validates :password,
                 length: { minimum: 4 },
                 if: -> { new_record? || !password.nil?}
